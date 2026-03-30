752c40024ac2fec2d427056ba6408c12bf1b3309
konradc67 <179463@stud.prz.edu.pl>
2026-03-30 20:59:48 +0200

    feat: add User model with fields for user management

--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.conf import settings
 import uuid
+from django.db import models
+
+class User(models.Model):
+    # Mapowanie kolumn dokładnie tak, jak masz w Neonie
+    users_id = models.AutoField(primary_key=True)
+    nickname = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    user_type = models.TextField()
+
+    class Meta:
+        db_table = 'users'  # Wymusza na Django szukanie tej konkretnej nazwy tabeli
+        managed = False     # BARDZO WAŻNE: Wyłącza migracje Django dla tej tabeli
+
+    def __str__(self):
+        return self.nickname
 
 class Quiz(models.Model):
     topic = models.CharField(max_length=50)
