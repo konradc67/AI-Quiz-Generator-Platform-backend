d082129d011dbad9aba00768a3e34c5c83a47ca9
konradc67 <179463@stud.prz.edu.pl>
2026-03-30 21:16:51 +0200

    refactor: update User model to extend AbstractUser and define roles

--- users/models.py
+++ users/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-class User(models.Model):
-    # Mapowanie kolumn dokładnie tak, jak masz w Neonie
-    users_id = models.AutoField(primary_key=True)
-    nickname = models.TextField()
-    email = models.TextField()
-    password = models.TextField()
-    user_type = models.TextField()
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = 'STUDENT', 'Uczeń'
+        TEACHER = 'TEACHER', 'Nauczyciel'
+        COLLEGIAN = 'COLLEGIAN', 'Student'
+        OTHER = 'OTHER', 'Inne'
 
-    class Meta:
-        db_table = 'users'  # Wymusza na Django szukanie tej konkretnej nazwy tabeli
-        managed = False     # BARDZO WAŻNE: Wyłącza migracje Django dla tej tabeli
-
-    def __str__(self):
-        return self.nickname
+    country = models.CharField(max_length=50)
+    premium = models.BooleanField(default=False)
+    points = models.PositiveIntegerField(default=0)
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.STUDENT
+    )
