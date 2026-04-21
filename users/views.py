3977d1302c030042260fe0dfecfba2b869c9553a
wiktoria034 <177154@stud.prz.edu.pl>
2026-04-21 16:44:19 +0200

    fix: update profile page endpoint

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, serializers
-from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
@@ -36,5 +36,13 @@ def user(request):
     return Response({"message": "user page"})
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def profile(request):
-    return Response({"message": "profile page"})
+    user = request.user
+    return Response({"message": "profile page",
+                     "username": user.username,
+                     "first_name": user.first_name,
+                     "last_name": user.last_name,
+                     "email": user.email,
+                     "country": user.country,
+                     "role": user.role,})
