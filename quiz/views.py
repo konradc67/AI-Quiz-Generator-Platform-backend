a15d1d7c31f2007b60cdc82d25bfed0aa7344b07
konradc67 <179463@stud.prz.edu.pl>
2026-04-30 22:41:04 +0200

    feat: adding a option for quest to generate a quiz and adjusting a AI prompt

--- quiz/views.py
+++ quiz/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .utils import get_ai_quiz, save_ai_quiz
 import traceback
 
@@ -11,26 +11,28 @@ def home(request):
     return render(request, 'quiz/home.html')
 
 class QuizGenerateView(APIView):
-    permission_classes=[AllowAny]
+    permission_classes = [AllowAny]
     
     def post(self, request):
         try:
             topic = request.data.get('prompt') or request.data.get('topic', 'General Knowledge')
-            question_count = request.data.get('questionCount', 10)
-            difficulty = request.data.get('difficulty', 'medium')
-            
-            user = request.user if request.user.is_authenticated else None
+            question_count = request.data.get('questionCount', 5)
+            difficulty = request.data.get('difficulty', 'easy')
             
             ai_data = get_ai_quiz(topic, question_count, difficulty)
             
             if isinstance(ai_data, dict) and "error" in ai_data:
                 return Response({"success": False, "error": ai_data}, status=400)
 
-            quiz = save_ai_quiz(topic, ai_data, user)
+            quiz_id = None
+            
+            if request.user.is_authenticated:
+                quiz = save_ai_quiz(topic, ai_data, request.user)
+                quiz_id = quiz.id
 
             return Response({
                 "success": True,
-                "quiz_id": quiz.id,
+                "quiz_id": quiz_id,  # Dla gościa będzie to po prostu null
                 "questions": ai_data
             })
 
